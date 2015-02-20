7952ea0fdd164d3808d86cf1a2f284f962632cb3
Naykoyan <nico861005@hotmail.com>
2015-02-20 14:13:30 -0500

    seeeeee

--- gib2/src/com/example/gib2/MainActivity.java
+++ gib2/src/com/example/gib2/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+//MALDITO GIB
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
