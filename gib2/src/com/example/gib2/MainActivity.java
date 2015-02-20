1f7fdfd00cabb68f504c798845bc79c638cc5a8a
Naykoyan <nico861005@hotmail.com>
2015-02-20 14:44:06 -0500

    Revert "SSSSS"
    This reverts commit bf078aa559ae7c1e4b37be4ced801e4448ecee6a.

--- gib2/src/com/example/gib2/MainActivity.java
+++ gib2/src/com/example/gib2/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends Activity {
 //	  ZZZZZZZZ
 	//juan
 	//Nicolas
-	//fffff
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
