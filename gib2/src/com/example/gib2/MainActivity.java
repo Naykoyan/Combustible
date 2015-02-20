9e983e129c3f2e412e69707f413f061087806962
juanpineda <jdpinedahernandez@gmail.com>
2015-02-20 14:48:40 -0500

    dfd

--- gib2/src/com/example/gib2/MainActivity.java
+++ gib2/src/com/example/gib2/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
 	//juan
 	//Nicolas
 	//fffff
+	//hola
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
